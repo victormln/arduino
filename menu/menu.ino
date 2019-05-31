02bf17b19c6a26ba3dd4f76983be03029b096d8a
victormln <victormln@users.noreply.github.com>
2019-05-31 12:52:25 +0200

    Removed unnecesary comments

--- menu/menu.ino
+++ menu/menu.ino
@@ -16,7 +16,6 @@ void setup() {
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
     if (isConfigurationButtonPushed() && !inMenu) {
         inMenu = true;
         printMenu();
